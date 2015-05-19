a5ca3213c0e895f8c167f3704defee6b9be7bf7f
Jacy Wang <jacywj@gmail.com>
2015-05-19 13:18:54 -0700

    Initialize repo

---
+++ ScrollViewPart2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewPart2
+//
+//  Created by JIAN WANG on 5/19/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewPart2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewPart2
+//
+//  Created by JIAN WANG on 5/19/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
